7678e716c8b092a44dbc02dfd2365f90c8f5d638
Lee Jones <scribblethink@gmail.com>
2010-11-19 12:14:35 -0500

    Update for Rails 3

--- lib/routes.rb
+++ lib/routes.rb
@@ -1,9 +1,22 @@
 module Pulse
   module Routes
-    def pulse(path)
-      connect path, :controller => 'pulse', :action => 'pulse'
+    module Rails2
+      def pulse(path)
+        connect path, :controller => 'pulse', :action => 'pulse'
+      end
+    end
+    module Rails3
+      def pulse(path)
+        match path, :controller => 'pulse', :action => 'pulse'
+      end
     end
   end
 end
 
-ActionController::Routing::RouteSet::Mapper.send :include, Pulse::Routes
+if ActionPack::VERSION::MAJOR == 2
+  ActionController::Routing::RouteSet::Mapper.send :include, Pulse::Routes::Rails2
+elsif ActionPack::VERSION::MAJOR == 3
+  ActionDispatch::Routing::Mapper.send :include, Pulse::Routes::Rails3
+else
+  # teh future
+end
